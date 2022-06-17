5a2026dfbb9ee40bda86b7b1971dea53848e7acc
Jander Teodoro <jandertheodoro@gmail.com>
2022-06-16 23:55:35 -0300

    finishing the documentation

--- src/adapter/searchCep.js
+++ src/adapter/searchCep.js
@@ -6,6 +6,7 @@ async function searchCep(city, axios, endpoint){
     try {
         const response = await axios.get(url);
         const cep = response.data[0].cep.replace('-', '');
+        console.log(response)
         return cep
     } catch(error){
         console.error(error);
